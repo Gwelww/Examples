0b6ea8359929d974d6c4af71015e8b1d575a7c31
Angy <rosa_02@mail.ru>
2023-09-27 02:37:53 +0300

    Special username

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == " алина")
+{
+    Console.WriteLine("Ура, это же Жабка!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
